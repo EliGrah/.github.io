7334c494e7208d289c1e851626f74747d707ff1b
EliGrah <97852074+EliGrah@users.noreply.github.com>
2022-11-08 11:21:32 -0700

    ica12

--- assignments/assignment_12/assignment_12.js
+++ assignments/assignment_12/assignment_12.js
@@ -6,4 +6,34 @@ button.addEventListener('click', updateOutput);
 
 function updateOutput() {
     output.textContent = phone_content.value;
+    console.log(output.textContent); //am i dumb?
 }
+
+
+//input binary for first 3 numbers
+parseInt(x, 2); //converts binary to decimal
+
+//"oh no! we didn't make enough room for the rest of the number, pls left shift your binary however many times you need"
+//"times the input by 2^_?"
+
+x = x << input; //do parse int after
+
+//binary getting boring lets switch over to hex
+if(input == 0){"no cheating!"}
+//Oh, pls shift this one over as well 
+y = y << input;
+
+parseInt(y, 16)
+
+z = x + y;
+//are you happy with your number so far?
+//display z
+//yes or no
+if(no){"sigh lets start over"}
+if(yes)
+//great! now, user feedback has let us know this procsss has been a little too complicated so we're going to simplify it down 
+//please incrment your last number by clicking
+
+//random increase by random(1-10)
+
+//went too far? click this button to divide by half
